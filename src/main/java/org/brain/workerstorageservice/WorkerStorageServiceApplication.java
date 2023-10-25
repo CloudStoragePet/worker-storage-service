f22bc88d6fc57782e0a467c90b13eb9e5c29b37a
Vitalii Seniuk <vitaliiseniuk04@gmail.com>
2023-10-25 20:23:40 +0300

    init Commit

---
+++ src/main/java/org/brain/workerstorageservice/WorkerStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package org.brain.workerstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerStorageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/brain/workerstorageservice/WorkerStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brain.workerstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
