59016bb3089bbb56c9ac122c38fba03f6ec9e998
Vitalii Seniuk <vitaliiseniuk04@gmail.com>
2023-10-30 19:52:35 +0200

    Configure rabbitMQ

--- src/main/java/org/brain/workerstorageservice/config/RabbitMQConfiguration.java
+++ src/main/java/org/brain/workerstorageservice/config/RabbitMQConfiguration.java
@@ -2,27 +2,39 @@ package org.brain.workerstorageservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class RabbitMQConfiguration {
+    private final RabbitQueueProperties queueProperties;
 
+    /**
+     * spring bean to create rabbitmq queue
+     **/
     @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public Queue queue() {
+        return new Queue(queueProperties.moveQueueName(), true);
     }
 
+    /**
+     * define exchange
+     **/
     @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
+    public TopicExchange exchange() {
+        return new TopicExchange(queueProperties.exchangeName());
+    }
+
+    /**
+     * bind queue to exchange
+     **/
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(queueProperties.routingKey());
     }
 
     // Created automatically by autoconfig-> ConnectionFactory, RabbitTemplate, RabbitAdmin
